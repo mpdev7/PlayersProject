c96155a08c621f040ffc000d45f5043342dc391c
mpdev7 <matteopedonese.dev@gmail.com>
2016-03-30 10:58:55 +0200

    Added two buttons for dinamically hide and show the add player and add list form. The buttons class dinamically change with ng-class

--- PlayersProject/js/ctrlPlayersList.js
+++ PlayersProject/js/ctrlPlayersList.js
@@ -8,8 +8,10 @@
     ctrlPlayersList.$inject = ['$scope', 'sList','$interval']; 
 
     function ctrlPlayersList($scope, sList, $interval) {
-        $scope.title = 'Players List';        
-        
+        $scope.title = 'Players List';
+        $scope.position = ["Goalkeeper", "Cornerback", "Midfielder", "Wing", "Forward"];
+
+        //Fetch player list from the server
         var playerlist = function() {
             var promise = sList.GetPlayersList();
             promise.then(function (response) {
@@ -25,16 +27,18 @@
             });
         }
               
+        //Add new player to list 
         var AddPlayer = function AddPlayer (){
             var promise = sList.ListPost($scope.obj.name, $scope.obj.surname, $scope.obj.position, $scope.obj.team);
             promise.then(function (response) {
                 playerlist();
                 $scope.insertForm.$setPristine();
                 $scope.obj = {};
+                defaultPositionSelect();
                 $scope.ErrorView = false;
             }, function (error) {
                 if (error == 409) (
-                    $scope.ErrorPost = "Wrong Request: " + $scope.obj.surname + " it's already in List!",
+                    $scope.ErrorPost = "Wrong Request: this player it's already in List!",
                     $scope.ErrorView = true,
                     $interval(function(){
                         $scope.ErrorView = false;
@@ -50,6 +54,7 @@
             });
         }
 
+        //Fetch player of the selected list
         var MyPlayersList = function (id) {
             var promise = sList.GetMyPlayers(id);
             promise.then(function (response) {
@@ -57,6 +62,7 @@
             });
         }
         
+        //Select the player from the list and put it on the personal list selected
         function ToMyList(idPlayer, idList, surname) {
             var promise = sList.MyListPost(idPlayer, idList);
             promise.then(function (response) {
@@ -64,7 +70,7 @@
                 playerlist();
             }, function (error) {
                     if (error == 409) (
-                    $scope.ErrorPost = "Wrong Request: " + surname + " it's already in List!",
+                    $scope.ErrorPost = "Wrong Request: this player it's already in List!",
                     $scope.ErrorView = true,
                     $interval(function () {
                         $scope.ErrorView = false;
@@ -80,6 +86,7 @@
             });
         }
 
+        //Add new personal list
         function AddList() {
             var promise = sList.AddList($scope.newlist);
 
@@ -98,12 +105,13 @@
             });
         }
 
+        //Fetch the personal lists
         function GetLists() {
             var promise = sList.GetLists();
 
             promise.then(function (response) {
                 $scope.lists = response;
-                defaultSelect();
+                defaultListSelect();
             }, function (error) {
                 if (error == 500) {
                     $scope.ErrorPost = "Wrong Get Request, status: 500",
@@ -115,6 +123,7 @@
             });
         }
 
+        //Remove player from the personal list
         function RemovePlayer(idPlayer, idList) {
             var promise = sList.RemoveMyPlayer(idPlayer, idList);
             promise.then(function (response) {
@@ -123,6 +132,7 @@
             });
         }
 
+        //Filter the player list 
         function filter(name) {
             if ($scope.filterOf == name) {
                 $scope.filterOf = "-" + name;
@@ -130,16 +140,48 @@
             else $scope.filterOf = name;
         }
 
-        function defaultSelect() {
+        //Selected the first list from the personal lists
+        function defaultListSelect() {
             if ($scope.lists != undefined) {
                 $scope.lSelect = $scope.lists[0].Id;
                 MyPlayersList($scope.lists[0].Id);
             }
         }
 
+        //Select the first position from the position options 'Goalkeeper'
+        function defaultPositionSelect() {
+            $scope.obj = { "position": $scope.position[0] };
+        }
+
         playerlist();
         GetLists();
+        defaultPositionSelect();
         
+        //Dinamically change of the button class 'Add player'
+        function changeMyListClass() {
+            if ($scope.myListClass == "btn btn-danger") {
+                $scope.myListClass = "btn btn-success";
+                $scope.myListSpanClass = "glyphicon glyphicon-plus-sign";
+            }
+            else
+            {
+                $scope.myListClass = 'btn btn-danger';
+                $scope.myListSpanClass = "glyphicon glyphicon-remove-sign";
+            }
+        }
+
+        //Dinamically change of the button class 'Add list'
+        function changeListClass() {
+            if ($scope.btnListClass == 'btn btn-success') {
+                $scope.btnListClass = 'btn btn-danger';
+                $scope.btnListSpanClass = 'glyphicon glyphicon-remove-sign';
+            }
+            else {
+                $scope.btnListClass = 'btn btn-success';
+                $scope.btnListSpanClass = 'glyphicon glyphicon-plus-sign';
+            }
+        }
+
         $scope.lSelect = null;
         $scope.AddPlayer = AddPlayer;
         $scope.ToMyList = ToMyList;
@@ -149,6 +191,12 @@
         $scope.ErrorView = false;
         $scope.filter = filter;
         $scope.filterOf = '';
-        $scope.obj = {};
+
+        $scope.myListClass = 'btn btn-success';
+        $scope.myListSpanClass = 'glyphicon glyphicon-plus-sign';
+        $scope.btnListClass = 'btn btn-success';
+        $scope.btnListSpanClass = 'glyphicon glyphicon-plus-sign';
+        $scope.changeMyListClass = changeMyListClass;
+        $scope.changeListClass = changeListClass;
     }
 })();
